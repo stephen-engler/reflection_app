2709d041b643b878fa20775bce5b21541f35d340
Stephen Engler <sengler1989@gmail.com>
2018-04-20 16:29:09 -0500

    add input fields and click handlers

--- src/components/add-page/AddPage.js
+++ src/components/add-page/AddPage.js
@@ -1,15 +1,88 @@
 import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import {withStyles} from 'material-ui/styles';
+import { connect } from 'react-redux';
 
 
+const styles = theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        backgroundColor: theme.palette.background.paper,
+
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 200,
+        backgroundColor: theme.palette.background.default,
+
+    },
+    menu: {
+        width: '100%',
+
+    },
+});
 
 class AddPage extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            userInput: {
+                topic: '',
+                reflection: '',
+            }
+        }
+    }
+
+    handleChangeFor = (type) => {
+        return (event) => {
+            this.setState({
+                userInput: {
+                    ...this.state.userInput,
+                    [type]: event.target.value
+                }
+            })
+        }
+    }
+
+    addNewReflection = ()=>{
+        this.props.dispatch({type: 'ADD_REF', payload: this.state.userInput})
+        this.setState({
+            userInput:{
+                topic: '',
+                reflection: '',
+            }
+        })
+    }
+
+
     render() {
         return (
-            <div className="App">
-                <p>add page</p>
+            <div className={this.props.classes.container}>
+                <input 
+                    type="text" 
+                    value={this.state.userInput.topic} 
+                    placeholder="topic"
+                    onChange={this.handleChangeFor('topic')}
+                />
+                <input 
+                    type="text" 
+                    value={this.state.userInput.reflection} 
+                    placeholder="reflection"
+                    onChange={this.handleChangeFor('reflection')}
+                />
+                <button onClick={this.addNewReflection}>
+                    Submit
+                </button>
+
             </div>
         );
     }
 }
 
-export default AddPage;
+const mapStateToProps = state => ({
+    state,
+});
+
+export default connect(mapStateToProps)(withStyles(styles)(AddPage));
--- src/index.js
+++ src/index.js
@@ -3,6 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import {
+    Provider
+} from 'react-redux';
+import {
+    createStore,
+    combineReducers,
+    applyMiddleware
+} from 'redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import {
+    takeEvery,
+    call,
+    put
+} from 'redux-saga/effects';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+
+function* rootSaga(){
+    yield takeEvery('ADD_REF', addRefSaga);
+}
+
+function* addRefSaga(action){
+    yield console.log('in addrefsaga')
+}
+
+
+const reflectionList = (state=[], action) =>{
+    return state;
+}
+
+const store = createStore(
+    combineReducers({reflectionList}),
+    applyMiddleware(sagaMiddleware, logger)
+)
+
+sagaMiddleware.run(rootSaga);
+
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
 registerServiceWorker();
